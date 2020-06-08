2411943c8fd1932ec6966060e39ef666d593e8c5
Casey <CaseyChapman@users.noreply.github.com>
2020-06-08 22:20:23 +0100

    Add project files.

---
+++ CasKingdomDestruction/SubModule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace CasKingdomDestruction
+{
+    public class SubModule : MBSubModuleBase
+    {
+		public override void OnGameInitializationFinished(Game game)
+		{
+			base.OnGameInitializationFinished(game);
+			if (Campaign.Current != null)
+			{
+				CampaignEvents.WeeklyTickEvent.AddNonSerializedListener(this, delegate
+				{
+					this.WeeklyTick();
+				});
+			}
+		}
+
+		public void WeeklyTick()
+		{
+			foreach (Kingdom kingdom in from t in Kingdom.All
+										where !t.IsEliminated && t.RulingClan != Clan.PlayerClan
+										select t)
+			{
+				if (!kingdom.Fiefs.Any<Town>())
+				{
+					foreach (Clan clan in kingdom.Clans.ToList<Clan>())
+					{
+						ChangeKingdomAction.ApplyByLeaveKingdom(clan, true);
+					}
+					DestroyKingdomAction.Apply(kingdom);
+				}
+			}
+		}
+	}
+
+}
